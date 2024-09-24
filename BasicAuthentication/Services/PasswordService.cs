4b26f27a23ce1c40d40046244124131abd2ed31b
mehdijafarimj <mehdij807@gmail.com>
2024-09-24 16:24:37 +0330

    deleted this file it was not nessecary for now

---
+++ BasicAuthentication/Services/PasswordService.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BasicAuthentication.Domain;
+
+public class PasswordService
+{
+    private const string PasswordPattern = @"^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$";
+
+    public bool IsValidPassword(string password)
+    {
+        return Regex.IsMatch(password, PasswordPattern);
+    }
+}
