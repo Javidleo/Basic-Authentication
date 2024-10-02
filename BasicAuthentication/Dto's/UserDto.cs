45d57f7bcadc70c93da61a4e1aebe0434ac78577
mehdijafarimj <mehdij807@gmail.com>
2024-10-02 14:52:51 +0330

    moving password hash class to userdto class and changed it to a method

--- BasicAuthentication/Dto's/UserDto.cs
+++ BasicAuthentication/Dto's/UserDto.cs
@@ -1,11 +1,17 @@
 ﻿using BasicAuthentication.Domain;
+using Microsoft.IdentityModel.Tokens;
+using System.Text.RegularExpressions;
 
 namespace BasicAuthentication.Dto_s;
 
 public class UserDto
 {
-    public string UserName { get; set; }    
+    public string UserName { get; set; }
     public string Password { get; set; }
+
+    private const string PasswordPattern = @"^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$";
+    public bool IsValidPassword(string password)
+    {
+        return Regex.IsMatch(Password, PasswordPattern);
+    }
 }
-
-
